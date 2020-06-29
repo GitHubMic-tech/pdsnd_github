61ad72b36316b315ddfd356585a549aab669a0ce
Michele Schivazappa <schivalo@libero.it>
2020-06-29 17:27:57 +0200

    feat: Files for git repo

---
+++ Bike-share-Question-1.R
@@ -0,0 +1,26 @@
+getwd()
+list.files()
+chi=read.csv('chicago.csv')
+ny=read.csv('new-york-city.csv')
+wa=read.csv('washington.csv', sep = ',')
+
+
+# Add datasets one after another
+rbind(chi,ny)
+names(chi)
+names(wa)
+
+install.packages("ggplot2")
+
+count <- table(chi$Start.Station)
+
+
+x=tail(sort(count),10)
+barplot(x, main="Top 10 starting stations in Chicago, IL",
+        ylab="Number of uses, in figures", las=2, col = 'blue')
+par(mar=c(10,2,1,1))
+
+names(x)
+
+x[names(x)]
+
