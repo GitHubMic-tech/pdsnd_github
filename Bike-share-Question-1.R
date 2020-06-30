ffb5a09d2f18f6dc15a439980f15e75b34f73d1f
Michele Schivazappa <schivalo@libero.it>
2020-06-30 10:43:08 +0200

    refactor: Change in chart bars color

--- Bike-share-Question-1.R
+++ Bike-share-Question-1.R
@@ -17,7 +17,7 @@ count <- table(chi$Start.Station)
 
 x=tail(sort(count),10)
 barplot(x, main="Top 10 starting stations (Chicago, IL)",
-        ylab="Number of uses", las=2, col = 'blue')
+        ylab="Number of uses", las=2, col = 'green')
 par(mar=c(10,2,1,1))
 
 names(x)
